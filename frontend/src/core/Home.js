671f8a2e09a6d53f7228cbb8083c2bedf80b9548
Charu Agrawal <53303041+charu-24@users.noreply.github.com>
2020-06-26 21:53:55 +0530

    frontend

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import "../styles.css"
+import { API } from "../user/backend"
 
 export default function Home() {
+    console.log("API IS", process.env.REACT_APP_DATABASE)
+
     return (
         <div>
             <h1> hey homy sweety </h1>
