b817268f84c2a8f346c51e96515af3edd9de7a7e
Charu Agrawal <53303041+charu-24@users.noreply.github.com>
2020-06-27 00:32:26 +0530

    Update Home.js

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import "../styles.css"
 import { API } from "../user/backend"
-
+import Base from "../core/Base"
 export default function Home() {
-    console.log("API IS", process.env.REACT_APP_DATABASE)
+    console.log("API IS", API)
 
     return (
-        <div>
-            <h1> hey homy sweety </h1>
+        <Base title="Home page">
+            <div className="row">
+            <div className="col-4">
+            <button className="btn btn-success">Test</button>
         </div>
+        <div className="col-4">
+            <button className="btn btn-success">Test</button>
+        </div>
+        <div className="col-4">
+            <button className="btn btn-success">Test</button>
+        
+        </div>
+            </div>
+        </Base>
     )
 }
