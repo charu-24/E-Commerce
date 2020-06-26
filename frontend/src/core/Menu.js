f7df8a1d1bddf693ce46cb8e77bf654c7621418c
Charu Agrawal <53303041+charu-24@users.noreply.github.com>
2020-06-27 00:32:32 +0530

    Create Menu.js

---
+++ frontend/src/core/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link, withRouter} from "react-router-dom"
+const Menu = () => (
+    <div>
+        <ul className="nav nav-tabs bg-dark">
+            <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+            </li>
+        </ul>
+    </div>
+
+)
+
+
+export default withRouter(Menu)
