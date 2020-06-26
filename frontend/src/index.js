aeb9d6ae1243ac81ccbcd25314c02af39fe7ec3d
Charu Agrawal <53303041+charu-24@users.noreply.github.com>
2020-06-27 00:32:01 +0530

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,5 +3,4 @@ import Routes from "./Routes"
 import ReactDOM from "react-dom"
 
 ReactDOM.render(
-    <Routes /> , document.getElementById("root")
-)
+    <Routes /> , document.getElementById("root"))
