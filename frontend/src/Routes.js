01985ee3ebbf88e270ee807f7fe23761aa52924f
Charu Agrawal <53303041+charu-24@users.noreply.github.com>
2020-06-27 17:21:29 +0530

    styling

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {Route, Link, Switch, BrowserRouter as Router} from "react-router-dom"
 import Home from "./core/Home"
+import Signup from './user/Signup'
+import Signin from './user/Signin'
 
 export default function Routes() {
     return (
         <Router>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/signup"  component={Signup} />
+                <Route path="/signin"  component={Signin} />
             </Switch>
         </Router>
     )
