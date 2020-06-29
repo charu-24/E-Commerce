53ba593d4ab7b1e1f594fd6353e86dce40b28d6a
Charu Agrawal <53303041+charu-24@users.noreply.github.com>
2020-06-30 02:09:58 +0530

    updated

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import {Route, Link, Switch, BrowserRouter as Router} from "react-router-dom"
-import Home from "./core/Home"
-import Signup from './user/Signup'
-import Signin from './user/Signin'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./core/Home";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
 
-export default function Routes() {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/signup"  component={Signup} />
-                <Route path="/signin"  component={Signin} />
-            </Switch>
-        </Router>
-    )
-}
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup"  component={Signup} />
+        <Route path="/signin" component={Signin} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
+
