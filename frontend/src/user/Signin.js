1f48afcfc97f50bd7aac0a22efb68bfe644a0127
Charu Agrawal <53303041+charu-24@users.noreply.github.com>
2020-06-30 21:03:53 +0530

    Update Signin.js

--- frontend/src/user/Signin.js
+++ frontend/src/user/Signin.js
@@ -1,8 +1,60 @@
-import React, { userState } from "react"
+import React, {useState } from "react"
 import Base from "../core/Base"
-import { Link } from "react-router-dom"
+import { Link, Redirect } from "react-router-dom"
+import {signin, authenticate, isAutheticated} from "../auth/helper/index"
+
+
 
 const Signin = () => {
+
+    const [values, setValues] = useState({
+        email:"",
+        encry_password:"",
+        error:"",
+        loading:false,
+        didRedirect:false
+    })
+    const {email, encry_password, error, loading, didRedirect} = values
+
+    const { user } = isAutheticated()
+
+    const handleChange = name => event => {
+        setValues({...values, error:false, [name]: event.target.value})
+    }
+
+    const onSubmit = event => {
+        event.preventDefault()
+        setValues({...values, error: false, loading:true})
+        signin({email,encry_password})
+        .then(data => {
+            if(data.error) {
+                setValues({...values, error:data.error, loading: false})
+            }else{
+                authenticate(data, () => {
+                    setValues({
+                        ...values,
+                        didRedirect:true,
+
+                    })
+                })
+            }
+        })
+        .catch(error => console.log("error in siginin"))
+    }
+
+    const performRedirect = () => {
+        if(didRedirect){
+            if( user && user.role===1){
+                return <p>redirected to admin</p>
+            }else{
+                return <p>redirected to user</p>
+            }
+        }
+        if(isAutheticated()){
+            return <Redirect to="/" />
+        }
+    }
+
     const signinform = () => {
         return (
             <div className="row">
@@ -13,15 +65,23 @@ const Signin = () => {
                             <label className="text-light">
                                 Email
                             </label>
-                            <input className="form-control" type="email" />
+                            <input className="form-control" 
+                            onChange={handleChange("email")}
+                            value={email}
+                            type="email" />
                         </div>
                         <div className="form-group">
                             <label className="text-light">
                                 Password
                             </label>
-                            <input className="form-control" type="password" />
+                            <input className="form-control" 
+                            onChange={handleChange("email")}
+                            value={email}
+                            type="password" />
                         </div>
-                        <button className="btn btn-success btn-block">Signup</button>
+                        <button 
+                        onClick={onSubmit}
+                        className="btn btn-success btn-block">Signup</button>
                     
                     </form>
                 </div>
@@ -30,11 +90,38 @@ const Signin = () => {
         )
 }
 
+const loadingMessage = () => {
+    return (
+      loading && (
+          <div className="alert alert-info">
+            <h1>Loading...</h1>
+          </div>
+      )
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+
 
     return(
         <Base title="Signin Page" description="Login user">
        
             {signinform()}
+            {performRedirect()}
         
         </Base>
     )
