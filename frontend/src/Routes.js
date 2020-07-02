1d99a0af17d60ed618ffcefd029dd77cdadf035b
Charu Agrawal <53303041+charu-24@users.noreply.github.com>
2020-07-02 17:23:23 +0530

    updated

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,11 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./core/Home";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
+import AdminRoute from "./auth/helper/AdminRoutes"
+import PrivateRoute from "./auth/helper/PrivateRoutes"
+import UserDashboard from "./user/UserDashBoard";
+import AdminDashboard from "./user/AdminDashBoard";
+
 
 const Routes = () => {
   return (
@@ -11,8 +16,10 @@ const Routes = () => {
         <Route path="/" exact component={Home} />
         <Route path="/signup"  component={Signup} />
         <Route path="/signin" component={Signin} />
+        <PrivateRoute path="/user/dashboard" component={UserDashboard}  />
+        <AdminRoute path="/admin/dashboard" component={AdminDashboard} />
       </Switch>
-    </BrowserRouter>
+1/4    </BrowserRouter>
   );
 };
 
--- frontend/src/core/Menu.js
+++ frontend/src/core/Menu.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, withRouter} from "react-router-dom"
-import { signout, isAutheticated } from '../auth/helper'
+import { isAutheticated, signout } from '../auth/helper'
 
 
 
@@ -27,16 +27,20 @@ const Menu = ({ history }) => (
                 style = { currentTab(history, "/cart") }
                 className="nav-link" to="/cart">Cart</Link>
             </li>
-            <li className="nav-item">
+            {isAutheticated() && isAutheticated().user.role ===0 && (
+                <li className="nav-item">
                 <Link
                 style = { currentTab(history, "/user/dashboard") }
-                className="nav-link" to="/user/dashboard">Dashboard</Link>
+                className="nav-link" to="/user/dashboard">U. Dashboard</Link>
             </li>
-            <li className="nav-item">
+            )}
+            {isAutheticated() && isAutheticated().user.role ===1 && (
+                <li className="nav-item">
                 <Link
                 style = { currentTab(history, "/admin/dashboard") }
                 className="nav-link" to="/admin/dashboard">A. Dashboard</Link>
             </li>
+            )}
             {!isAutheticated() && (
                 <li className="nav-item">
                 <Link 
