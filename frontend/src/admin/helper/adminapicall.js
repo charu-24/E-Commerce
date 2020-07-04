2b2b57c27c0c557a612db20495626b5b76d07987
Charu Agrawal <53303041+charu-24@users.noreply.github.com>
2020-07-04 19:14:16 +0530

    Update adminapicall.js

--- frontend/src/admin/helper/adminapicall.js
+++ frontend/src/admin/helper/adminapicall.js
@@ -1,5 +1,8 @@
 const { API } = require("../../backend");
 
+
+
+//category calls
 export const createCategory = (userId, token, category) =>{
     return fetch(`${API}/category/create/${userId}`, {
         method: "POST",
@@ -13,4 +16,96 @@ export const createCategory = (userId, token, category) =>{
     )
     .then(response =>{ return response.json()})
     .catch(err => console.log(err))
+}
+
+//get all categories
+export const getAllCategory = () => {
+    return fetch(`${API}/categories`, {
+        method: "GET"
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+
+
+//products calls
+
+export const createProduct =(userId, token,product) => {
+    return fetch(`${API}/product/create/${userId}`, {
+        method: "POST",
+        headers:{
+            Accept: "application/json",
+           
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+//get all products
+export const getAllProducts = () => {
+    return fetch(`${API}/products`, {
+        method: "GET"
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+//delete a product
+export const deleteProduct =(productId, userId, token) =>{
+    return fetch(`{API}/product/${productId}/${userId}`, {
+        method: "DELETE",
+        headers:{
+            Accept: "application/json",
+           
+            Authorization: `Bearer ${token}`
+        }
+
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+
+
+//get a product
+export const getProduct = productId =>{
+    return fetch(`{API}/product/${productId}`, {
+        method: "GET"
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+//update a product
+export const updateProduct = (productId, userId, token, product) =>{
+    return fetch(`{API}/product/${productId}/${userId}`, {
+        
+        method: "POST",
+        headers:{
+            Accept: "application/json",
+           
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+
 }
